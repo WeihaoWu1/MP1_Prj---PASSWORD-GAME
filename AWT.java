cc4beee2e17e52147a3004bd6c66d382592cdefa
small <smallgiant1010@gmail.com>
2023-10-21 22:31:06 -0400

    IF NO COMMIT I SAD

--- AWT.java
+++ AWT.java
@@ -12,7 +12,9 @@ public class AWT {
     private Label requirements7 = new Label();
     private Label requirements8 = new Label();
     private Label requirements9 = new Label();
-//    private Label requirements10 = new Label();
+    private Label requirements10 = new Label();
+    private Label requirements11 = new Label();
+    private Label requirements12 = new Label();
     private Label yourPassword = new Label();
     private boolean met1;
     private boolean met2;
@@ -23,21 +25,37 @@ public class AWT {
     private boolean met7;
     private boolean met8;
     private boolean met9;
-//    private boolean met10;
+    private boolean met10;
+    private boolean met11;
+    private boolean met12;
     private String updatedText = "";
 
 
     public AWT() {
-        Font myFont = new Font("Roboto", Font.ITALIC | Font.BOLD, 12);
-        Font newFont = myFont.deriveFont(50F);
+        Font myFont1 = new Font("Roboto", Font.ITALIC | Font.BOLD, 12);
+        Font myFont2 = new Font("Monospaced", Font.BOLD, 12);
+        Font myFont3 = null;
+        Font newFont = myFont1.deriveFont(50F);
         TextField initialPassword = new TextField();
         initialPassword.setBounds(50, 100, 200, 50);
         Game firstAttempt = new Game(updatedText, 0);
+        int randomNumber = firstAttempt.getRandomNumber();
         Label titleLabel = new Label(firstAttempt.getIntroduction());
         titleLabel.setFont(newFont);
+        requirements1.setFont(myFont2);
+        requirements2.setFont(myFont2);
+        requirements3.setFont(myFont2);
+        requirements4.setFont(myFont2);
+        requirements5.setFont(myFont2);
+        requirements6.setFont(myFont2);
+        requirements7.setFont(myFont2);
+        requirements8.setFont(myFont2);
+        requirements9.setFont(myFont2);
+        requirements10.setFont(myFont2);
+        requirements11.setFont(myFont2);
+        requirements12.setFont(myFont2);
         System.out.println(firstAttempt);
-        titleLabel.setAlignment(Label.CENTER);
-        enterButton.setBounds(200, 400, 100, 100);
+        enterButton.setBounds(500, 500, 100, 100);
         enterButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -51,28 +69,39 @@ public class AWT {
                         met2 = firstAttempt.specialCharacter();
                         requirements2.setText("Must Include a Special Character: " + met2);
                         if (met2) {
-                            met3 = firstAttempt.classmateName();
-                            requirements3.setText("Must Include a Classmate's Name: " + met3);
-                            if (met3) {
-                                met4 = firstAttempt.capitalLetter();
-                                requirements4.setText("Must Include a Capital Letter: " + met4);
-                                if (met4) {
-                                    met5 = firstAttempt.season();
-                                    requirements5.setText("Must Include a Season: " + met5);
-                                    if (met5) {
-                                        met6 = firstAttempt.month();
-                                        requirements6.setText("Must Include a month:" + met6);
-                                        if (met6) {
-                                            met7 = firstAttempt.romanNumerals();
-                                            requirements7.setText("Must include the Roman Numerals of 25 at the end: " + met7);
-                                            if (met7) {
-                                                met8 = firstAttempt.randNumber();
-                                                requirements8.setText("Must include this random number " + "(" + firstAttempt.getDisplayNum() + "):" + met8);
-                                                if (met8) {
-                                                    met9 = firstAttempt.decimal();
-                                                    requirements9.setText("Must include a decimal value (WIP): " + met9);
-                                                    if (met9) {
-                                                        yourPassword.setText(firstAttempt.getPassword());
+                            met11 = firstAttempt.number();
+                            requirements11.setText("Must include a number: " + met11);
+                            if (met11) {
+                                met3 = firstAttempt.classmateName();
+                                requirements3.setText("Must Include a Classmate's Name: " + met3);
+                                if (met3) {
+                                    met4 = firstAttempt.capitalLetter();
+                                    requirements4.setText("Must Include a Capital Letter: " + met4);
+                                    if (met4) {
+                                        met5 = firstAttempt.season();
+                                        requirements5.setText("Must Include a Season: " + met5);
+                                        if (met5) {
+                                            met6 = firstAttempt.month();
+                                            requirements6.setText("Must Include a month:" + met6);
+                                            if (met6) {
+                                                met7 = firstAttempt.romanNumerals();
+                                                requirements7.setText("Must include the Roman Numerals of 25 at the end: " + met7);
+                                                if (met7) {
+                                                    met12 = firstAttempt.productFifty();
+                                                    requirements12.setText("Must have a product that is more 50 for all numbers: " + met12);
+                                                    if (met12) {
+                                                        met8 = firstAttempt.displayNum(randomNumber);
+                                                        requirements8.setText("Must include this random number " + "(" + randomNumber + "):" + met8);
+                                                      if (met12) {
+                                                          met9 = firstAttempt.decimal();
+                                                          requirements9.setText("Must include a decimal value (WIP): " + met9);
+                                                          if (met9) {
+                                                            met10 = firstAttempt.sumThirty();
+                                                            requirements10.setText("Must have a sum of 30 for all numbers: " + met10);
+                                                            if (met10) {
+                                                                yourPassword.setText(firstAttempt.getPassword());
+                                                            }
+                                                        }
                                                     }
                                                 }
                                             }
@@ -82,29 +111,46 @@ public class AWT {
                             }
                         }
                     }
-//                    met10 = firstAttempt.sumThirty();
-//                    requirements10.setText(String.valueOf(met10));
-
+                    else {
+                        requirements1.setText("");
+                        requirements2.setText("");
+                        requirements3.setText("");
+                        requirements4.setText("");
+                        requirements5.setText("");
+                        requirements6.setText("");
+                        requirements7.setText("");
+                        requirements8.setText("");
+                        requirements9.setText("");
+                        requirements10.setText("");
+                        requirements11.setText("");
+                        requirements12.setText("");
+                    }
+                }
                 }
             }
         });
         // Button
         requirements1.setBounds(50, 200, 1000, 15);
-        requirements2.setBounds(50, 220, 1000, 15);
-        requirements3.setBounds(50, 240, 1000, 15);
-        requirements4.setBounds(50, 260, 1000, 15);
-        requirements5.setBounds(50, 280, 1000, 15);
-        requirements6.setBounds(50, 300, 1000, 15);
-        requirements7.setBounds(50, 320, 1000, 15);
-        requirements8.setBounds(50, 340, 1000, 15);
-        requirements9.setBounds(50, 360, 1000, 15);
-//        requirements10.setBounds(50, 380, 1000, 15);
+        requirements11.setBounds(50, 220, 1000, 15);
+        requirements2.setBounds(50, 240, 1000, 15);
+        requirements3.setBounds(50, 260, 1000, 15);
+        requirements4.setBounds(50, 280, 1000, 15);
+        requirements5.setBounds(50, 300, 1000, 15);
+        requirements6.setBounds(50, 320, 1000, 15);
+        requirements7.setBounds(50, 340, 1000, 15);
+        requirements8.setBounds(50, 360, 1000, 15);
+        requirements9.setBounds(50, 380, 1000, 15);
+        requirements10.setBounds(50, 420, 1000, 15);
+        requirements12.setBounds(50, 400, 1000, 15);
+        titleLabel.setBounds(750, 100, 700, 50);
+
+
         // Update
         // Add + Action
-        //            mainFrame.setLayout(null);
         mainFrame.add(enterButton);
         mainFrame.add(initialPassword);
         mainFrame.add(requirements1);
+        mainFrame.add(requirements11);
         mainFrame.add(requirements2);
         mainFrame.add(requirements3);
         mainFrame.add(requirements4);
--- Game.java
+++ Game.java
@@ -22,7 +22,7 @@ public class Game {
             "preston", "toshinori", "raymond", "weihao", "linbin", "sumya"};
 
     private final String[] numbers = {"1", "2", "3", "4", "5", "6", "7", "8", "9"};
-    private int displayNum;
+    private int randomNumber;
     List<String> numList = Arrays.asList(numbers);
 
     // GAME METHOD
@@ -30,44 +30,48 @@ public class Game {
     public Game(String pass, int attemptNumber) {
         this.password = pass;
         this.attempts = attemptNumber;
+        randNumber();
     }
 
-    public Game(){
+    public Game() {
         System.out.println("Game is overloaded");
     }
 
-    public void updatePassword(String newPassword){
+    public void updatePassword(String newPassword) {
         password = newPassword;
     }
 
     // ACCESSOR METHODS
 
-    public String getPassword(){
+    public String getPassword() {
         return password;
     }
-    public String toString(){
+
+    public String toString() {
         return "Welcome to the Password Game!";
     }
-    public String getIntroduction(){
+
+    public String getIntroduction() {
         return introduction;
     }
-    public Integer getDisplayNum() {return displayNum;}
 
-    public int getSums(){
+    public int getSums() {
         return sums;
     }
+
     public int getAttempts() {
         return attempts;
     }
+    public int getRandomNumber() {return randomNumber;}
 
     //MISCELLANEOUS METHODS
-    public static double calculateScore(int attempts){
+    public static double calculateScore(int attempts) {
         double percentage = .000001;
-        int formula = (int)(percentage * Integer.MAX_VALUE * attempts + .5);
+        int formula = (int) (percentage * Integer.MAX_VALUE * attempts + .5);
         return Double.parseDouble((("Your Score is: " + (formula) + "and it took you " + attempts + "tries!")));
     }
 
-    public void getScore(int numAttempts){
+    public void getScore(int numAttempts) {
         System.out.println(Game.calculateScore(numAttempts));
     }
 
@@ -137,37 +141,35 @@ public class Game {
         return false;
     }
 
-    public boolean randNumber() { // RANDOM REQUIREMENT
-        int rand1 = (int) (Math.random() * (9 - 1 + 1) + 1);
-        displayNum = rand1;
-        String rand2 = String.format("%d", rand1);
-        if (password.contains(rand2)) {
+    public void randNumber() { // RANDOM REQUIREMENT
+        int rand1 = (int) (Math.random() * (9 - 69 + 69) + 1);
+        randomNumber = rand1;
+    }
+    public boolean displayNum(int random){
+        if (password.contains(String.valueOf(random))){
             return true;
         }
         return false;
     }
 
-    public boolean decimal() { // DECIMAL REQUIREMENT
-        for (int i = 0; i < numbers.length; i++) {
-            if (password.contains(numbers[i])) {
-                if ((password.charAt(i + 1) == '.')){
-                    if ((numList.contains(password.charAt(i + 2)))) {
-                        return true;
-                    }
-                    else if (password.charAt(i--) == '.') {
-                        return true;
-                    }
+    public boolean decimal(){
+        if(password.contains(".")) {
+            int z = password.indexOf(".");
+            char[] temporary = password.toCharArray();
+            for (int n = 0;n<numbers.length;n++) {
+                if (String.valueOf(temporary[z+1]).equals(numbers[n])) {
+                    return true;
                 }
             }
         }
         return false;
     }
-    public boolean productFifty(){
+    public boolean productFifty() {
         int product = 1;
-        if(password.contains("0")){
+        if (password.contains("0")) {
             return false;
         }
-        for (int i=0;i<password.length(); i++){
+        for (int i = 0; i < password.length(); i++) {
             for (int n = 0; n < numbers.length; n++) {
                 if (password.substring(i, i+=1).equals(numbers[n])) {
                     product *= n+1;
@@ -176,20 +178,23 @@ public class Game {
         }
         return true;
     }
+
     public boolean sumThirty() {        //SUM OF THIRTY REQUIREMENT
         int sum = 0;
         for (int i = 0; i < password.length(); i++) {
-            String y = String.valueOf(i + 1);
             for (int n = 0; n < numbers.length; n++) {
-                if (password.substring(i, i+=1).equals(numbers[n])) {
-                    sum += n+1;
+                int z = i;
+                if (!(i == password.length() - 1)) {
+                    z = i + 1;
+                }
+                if (password.substring(i, z).equals(numbers[n])) {
+                    sum += n + 1;
                 }
             }
         }
         if (sum == 30) {
             return true;
-        }
-        else {
+        } else {
             return false;
         }
     }
